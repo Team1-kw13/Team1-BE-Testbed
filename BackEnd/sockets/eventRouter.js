569e16e3cbb83ab71ed32ef01fe9be3ffe9fab69
leehyowon14 <leehyowon14@gmail.com>
2025-08-05 01:16:14 +0900

    feat: :sparkles: 웹소켓 이벤트 수신 구현

--- BackEnd/sockets/eventRouter.js
+++ BackEnd/sockets/eventRouter.js
@@ -5,6 +5,35 @@ function setupSocket(server) {
 
   io.on("connection", (socket) => {
     console.log("[socket] 연결됨:", socket.id);
+
+    socket.on("openai:conversation", (data) => {
+      const { type } = data;
+
+      switch (type) {
+        case "input_audio_buffer.commit":
+          console.log("발화 시작");
+          break;
+
+        case "input_audio_buffer.append":
+          console.log("오디오 버퍼 수신");
+          break;
+
+        case "input_audio_buffer.end":
+          console.log("발화 끝");
+          break;
+
+        default:
+          console.warn("알 수 없는 클라이언트 메시지:", type);
+      }
+    });
+
+    socket.on("sonju:summarize", (data) => {
+      console.log("요약 요청");
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`[socket] 연결 종료: ${socket.id}`);
+    });
   });
 }
 
